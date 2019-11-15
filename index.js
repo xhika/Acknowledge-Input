9d193edd524d5030b8e111363a57ceeaab3b4d2c
Xhika <laureta.dzika@gmail.com>
2019-11-15 09:34:18 +0100

    added serviceworker & manifest for pwa

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-'use strict';
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 
 // Create canvas
 const canvas = document.createElement('canvas');
